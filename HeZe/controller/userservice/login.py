72639589b83b5ce0a73646b689824d972bd8724f
yourname <liujiannan@imudges.com>
2016-10-18 16:18:28 +0800

    login

--- HeZe/controller/userservice/login.py
+++ HeZe/controller/userservice/login.py
@@ -1,5 +1,16 @@
 from HeZe.models import User
 
 
-def dologin(TelNumber, PassWord):
-    pass
+def dologin(UserPhone, PassWord):
+    u = User.objects.filter(UserPhone=UserPhone, PassWord=PassWord)
+    if not u:
+        state = 0
+        msg = '用户名或密码错误'
+    else:
+        state = 1
+        msg = '登陆成功'
+    array = {
+        'state': state,
+        'msg': msg
+    }
+    return array
